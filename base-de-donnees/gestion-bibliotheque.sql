493fe32284f34b92ec51d2610746bac74f003138
Reyes17 <89939796+Reyes17@users.noreply.github.com>
2023-08-11 00:16:29 +0200

    Mise à jour base de données

--- base-de-donnees/gestion-bibliotheque.sql
+++ base-de-donnees/gestion-bibliotheque.sql
@@ -1,18 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
-<<<<<<< HEAD
 -- Hôte : 127.0.0.1:3306
--- Généré le : mer. 26 juil. 2023 à 13:14
+-- Généré le : jeu. 10 août 2023 à 22:14
 -- Version du serveur :  5.7.31
 -- Version de PHP : 7.3.21
-=======
--- Hôte : localhost:8889
--- Généré le : mer. 26 juil. 2023 à 12:12
--- Version du serveur : 5.7.39
--- Version de PHP : 8.2.0
->>>>>>> f10f8276d1e4405201c719df1d0db3536357337f
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -25,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `gestion-bibliotheque`
+-- Base de données : `bibliotheque`
 --
 
 -- --------------------------------------------------------
@@ -34,14 +27,14 @@ SET time_zone = "+00:00";
 -- Structure de la table `auteur`
 --
 
-CREATE TABLE `auteur` (
-  `num_aut` int(11) NOT NULL,
+DROP TABLE IF EXISTS `auteur`;
+CREATE TABLE IF NOT EXISTS `auteur` (
+  `num_aut` int(11) NOT NULL AUTO_INCREMENT,
   `nom_aut` varchar(255) NOT NULL,
   `prenom_aut` varchar(255) NOT NULL,
   `creer_le` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `est_actif` int(11) NOT NULL DEFAULT '1',
   `est_supprimer` int(11) NOT NULL DEFAULT '0',
-<<<<<<< HEAD
   `maj_le` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`num_aut`)
 ) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
@@ -52,10 +45,6 @@ CREATE TABLE `auteur` (
 
 INSERT INTO `auteur` (`num_aut`, `nom_aut`, `prenom_aut`, `creer_le`, `est_actif`, `est_supprimer`, `maj_le`) VALUES
 (29, 'Th&eacute;riault', 'Sabrina', '2023-07-25 14:54:24', 1, 0, NULL);
-=======
-  `maj_le` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
->>>>>>> f10f8276d1e4405201c719df1d0db3536357337f
 
 -- --------------------------------------------------------
 
@@ -63,39 +52,19 @@ INSERT INTO `auteur` (`num_aut`, `nom_aut`, `prenom_aut`, `creer_le`, `est_actif
 -- Structure de la table `auteur_secondaire`
 --
 
-CREATE TABLE `auteur_secondaire` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `auteur_secondaire`;
+CREATE TABLE IF NOT EXISTS `auteur_secondaire` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `cod_ouv` int(11) DEFAULT NULL,
   `num_aut` int(11) DEFAULT NULL,
   `creer_le` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `est_actif` int(11) NOT NULL DEFAULT '1',
   `est_supprimer` int(11) NOT NULL DEFAULT '0',
-<<<<<<< HEAD
   `maj_le` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `num_aut` (`num_aut`),
   KEY `cod_ouv` (`cod_ouv`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-=======
-  `maj_le` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `categorie`
---
-
-CREATE TABLE `categorie` (
-  `cod_cat` int(11) NOT NULL,
-  `cod_dom` int(11) NOT NULL,
-  `nom_cat` varchar(255) NOT NULL,
-  `creer_le` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `supprimer_le` int(1) NOT NULL DEFAULT '0',
-  `maj_le` timestamp NULL DEFAULT NULL,
-  `est_actif` int(1) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
->>>>>>> f10f8276d1e4405201c719df1d0db3536357337f
 
 -- --------------------------------------------------------
 
@@ -103,15 +72,19 @@ CREATE TABLE `categorie` (
 -- Structure de la table `date_parution`
 --
 
-CREATE TABLE `date_parution` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `date_parution`;
+CREATE TABLE IF NOT EXISTS `date_parution` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `cod_ouv` int(11) NOT NULL,
   `cod_lang` int(11) NOT NULL,
   `dat_par` date NOT NULL,
   `creer_le` int(11) NOT NULL,
   `est_actif` int(11) NOT NULL,
   `est_supprimer` int(11) NOT NULL,
-  `maj_le` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `maj_le` timestamp NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `cod_ouv` (`cod_ouv`),
+  KEY `cod_lang` (`cod_lang`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -120,13 +93,13 @@ CREATE TABLE `date_parution` (
 -- Structure de la table `domaine`
 --
 
-CREATE TABLE `domaine` (
-  `cod_dom` int(11) NOT NULL,
+DROP TABLE IF EXISTS `domaine`;
+CREATE TABLE IF NOT EXISTS `domaine` (
+  `cod_dom` int(11) NOT NULL AUTO_INCREMENT,
   `lib_dom` varchar(255) NOT NULL,
   `creer_le` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `est_actif` int(11) NOT NULL DEFAULT '1',
   `est_supprimer` int(11) NOT NULL DEFAULT '0',
-<<<<<<< HEAD
   `maj_le` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`cod_dom`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
@@ -142,10 +115,6 @@ INSERT INTO `domaine` (`cod_dom`, `lib_dom`, `creer_le`, `est_actif`, `est_suppr
 (8, 'M&eacute;decine, sciences, techniques', '2023-07-25 13:36:32', 1, 0, '2023-07-25 13:36:32'),
 (9, 'Litt&eacute;rature', '2023-07-25 13:37:08', 1, 0, '2023-07-25 13:37:08'),
 (10, 'Scolaire &amp; p&eacute;dagogie', '2023-07-25 13:38:05', 1, 0, '2023-07-25 13:38:05');
-=======
-  `maj_le` timestamp NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
->>>>>>> f10f8276d1e4405201c719df1d0db3536357337f
 
 -- --------------------------------------------------------
 
@@ -153,21 +122,18 @@ INSERT INTO `domaine` (`cod_dom`, `lib_dom`, `creer_le`, `est_actif`, `est_suppr
 -- Structure de la table `domaine_ouvrage`
 --
 
-CREATE TABLE `domaine_ouvrage` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `domaine_ouvrage`;
+CREATE TABLE IF NOT EXISTS `domaine_ouvrage` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `cod_ouv` int(11) NOT NULL,
   `cod_dom` int(11) NOT NULL,
   `creer_le` int(11) NOT NULL,
   `est_actif` int(11) NOT NULL,
   `est_supprimer` int(11) NOT NULL,
-<<<<<<< HEAD
   `maj_le` timestamp NOT NULL,
   PRIMARY KEY (`id`),
   KEY `cod_ouv` (`cod_ouv`),
   KEY `cod_dom` (`cod_dom`)
-=======
-  `maj_le` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
->>>>>>> f10f8276d1e4405201c719df1d0db3536357337f
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -176,14 +142,17 @@ CREATE TABLE `domaine_ouvrage` (
 -- Structure de la table `emprunt`
 --
 
-CREATE TABLE `emprunt` (
-  `num_emp` int(11) NOT NULL,
+DROP TABLE IF EXISTS `emprunt`;
+CREATE TABLE IF NOT EXISTS `emprunt` (
+  `num_emp` int(11) NOT NULL AUTO_INCREMENT,
   `dat_emp` datetime NOT NULL,
   `id` int(11) NOT NULL,
   `creer_le` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `est_actif` int(11) NOT NULL,
   `est_supprimer` int(11) NOT NULL,
-  `maj_le` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+  `maj_le` timestamp NOT NULL,
+  PRIMARY KEY (`num_emp`),
+  KEY `num_mem` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -192,14 +161,27 @@ CREATE TABLE `emprunt` (
 -- Structure de la table `langue`
 --
 
-CREATE TABLE `langue` (
-  `cod_lang` int(11) NOT NULL,
+DROP TABLE IF EXISTS `langue`;
+CREATE TABLE IF NOT EXISTS `langue` (
+  `cod_lang` int(11) NOT NULL AUTO_INCREMENT,
   `lib_lang` varchar(255) NOT NULL,
   `creer_le` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `est_actif` int(11) NOT NULL DEFAULT '1',
   `est_supprimer` int(11) NOT NULL DEFAULT '0',
-  `maj_le` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `maj_le` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`cod_lang`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `langue`
+--
+
+INSERT INTO `langue` (`cod_lang`, `lib_lang`, `creer_le`, `est_actif`, `est_supprimer`, `maj_le`) VALUES
+(9, 'Fran&ccedil;ais', '2023-07-24 10:06:56', 1, 0, NULL),
+(10, 'Anglais', '2023-07-24 10:07:09', 1, 0, NULL),
+(11, 'Allemand', '2023-07-24 10:07:15', 1, 0, NULL),
+(12, 'Espagnol', '2023-07-24 10:07:18', 1, 0, NULL),
+(13, 'Grec', '2023-07-24 10:07:21', 1, 0, NULL);
 
 -- --------------------------------------------------------
 
@@ -207,8 +189,9 @@ CREATE TABLE `langue` (
 -- Structure de la table `membre_indelicat`
 --
 
-CREATE TABLE `membre_indelicat` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `membre_indelicat`;
+CREATE TABLE IF NOT EXISTS `membre_indelicat` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `num_emp` int(11) NOT NULL,
   `cod_ouv` int(11) NOT NULL,
   `dat_ret` datetime NOT NULL,
@@ -217,7 +200,10 @@ CREATE TABLE `membre_indelicat` (
   `creer_le` int(11) NOT NULL,
   `est_actif` int(11) NOT NULL,
   `est_supprimer` int(11) NOT NULL,
-  `maj_le` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `maj_le` timestamp NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `num_emp` (`num_emp`),
+  KEY `cod_ouv` (`cod_ouv`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -226,17 +212,20 @@ CREATE TABLE `membre_indelicat` (
 -- Structure de la table `ouvrage`
 --
 
-CREATE TABLE `ouvrage` (
-  `cod_ouv` int(11) NOT NULL,
+DROP TABLE IF EXISTS `ouvrage`;
+CREATE TABLE IF NOT EXISTS `ouvrage` (
+  `cod_ouv` int(11) NOT NULL AUTO_INCREMENT,
   `titre` varchar(255) NOT NULL,
   `nb_ex` int(11) NOT NULL,
-  `periodicite` varchar(255) NOT NULL,
+  `periodicite` varchar(255) DEFAULT NULL,
   `num_aut` int(11) NOT NULL,
-  `image` varchar(255) DEFAULT NULL,
+  `img` varchar(255) DEFAULT NULL,
   `creer_le` int(11) NOT NULL,
   `est_actif` int(11) NOT NULL,
   `est_supprimer` int(11) NOT NULL,
-  `maj_le` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `maj_le` timestamp NOT NULL,
+  PRIMARY KEY (`cod_ouv`),
+  KEY `num_aut` (`num_aut`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -245,16 +234,26 @@ CREATE TABLE `ouvrage` (
 -- Structure de la table `token`
 --
 
-CREATE TABLE `token` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `token`;
+CREATE TABLE IF NOT EXISTS `token` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `type` varchar(255) NOT NULL,
   `token` varchar(255) NOT NULL,
   `est_actif` tinyint(4) NOT NULL DEFAULT '1',
   `est_supprimer` tinyint(11) NOT NULL DEFAULT '0',
   `creer_le` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `maj_le` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `maj_le` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `token`
+--
+
+INSERT INTO `token` (`id`, `user_id`, `type`, `token`, `est_actif`, `est_supprimer`, `creer_le`, `maj_le`) VALUES
+(28, 31, 'VALIDATION_COMPTE', 'VALIDATION_COMPTE64b2a74c4f45b', 0, 1, '2023-07-15 14:03:56', '2023-07-15 12:08:46'),
+(29, 31, 'NOUVEAU_MOT_DE_PASSE', '64b2a85592c01', 0, 1, '2023-07-15 14:08:21', '2023-07-15 12:08:46');
 
 -- --------------------------------------------------------
 
@@ -262,8 +261,9 @@ CREATE TABLE `token` (
 -- Structure de la table `utilisateur`
 --
 
-CREATE TABLE `utilisateur` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `utilisateur`;
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(255) NOT NULL,
   `prenom` varchar(255) NOT NULL,
   `sexe` varchar(1) DEFAULT NULL,
@@ -280,172 +280,17 @@ CREATE TABLE `utilisateur` (
   `email_valide` int(11) DEFAULT NULL,
   `telephone_valide` int(11) DEFAULT NULL,
   `nom_utilisateur` varchar(255) NOT NULL,
-  `adresse` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `adresse` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
 
 --
--- Index pour les tables déchargées
+-- Déchargement des données de la table `utilisateur`
 --
 
---
--- Index pour la table `auteur`
---
-ALTER TABLE `auteur`
-  ADD PRIMARY KEY (`num_aut`);
-
---
--- Index pour la table `auteur_secondaire`
---
-ALTER TABLE `auteur_secondaire`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `num_aut` (`num_aut`),
-  ADD KEY `cod_ouv` (`cod_ouv`);
-
---
--- Index pour la table `categorie`
---
-ALTER TABLE `categorie`
-  ADD PRIMARY KEY (`cod_cat`),
-  ADD KEY `categorie_domaine_cod_dom` (`cod_dom`);
-
---
--- Index pour la table `date_parution`
---
-ALTER TABLE `date_parution`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `cod_ouv` (`cod_ouv`),
-  ADD KEY `cod_lang` (`cod_lang`);
-
---
--- Index pour la table `domaine`
---
-ALTER TABLE `domaine`
-  ADD PRIMARY KEY (`cod_dom`);
-
---
--- Index pour la table `domaine_ouvrage`
---
-ALTER TABLE `domaine_ouvrage`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `cod_ouv` (`cod_ouv`),
-  ADD KEY `cod_dom` (`cod_dom`),
-  ADD KEY `domaine_ouvrage_categorie_cod_cat` (`cod_cat`);
-
---
--- Index pour la table `emprunt`
---
-ALTER TABLE `emprunt`
-  ADD PRIMARY KEY (`num_emp`),
-  ADD KEY `num_mem` (`id`);
-
---
--- Index pour la table `langue`
---
-ALTER TABLE `langue`
-  ADD PRIMARY KEY (`cod_lang`);
-
---
--- Index pour la table `membre_indelicat`
---
-ALTER TABLE `membre_indelicat`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `num_emp` (`num_emp`),
-  ADD KEY `cod_ouv` (`cod_ouv`);
-
---
--- Index pour la table `ouvrage`
---
-ALTER TABLE `ouvrage`
-  ADD PRIMARY KEY (`cod_ouv`),
-  ADD KEY `num_aut` (`num_aut`);
-
---
--- Index pour la table `token`
---
-ALTER TABLE `token`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `utilisateur`
---
-ALTER TABLE `utilisateur`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `auteur`
---
-ALTER TABLE `auteur`
-  MODIFY `num_aut` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `auteur_secondaire`
---
-ALTER TABLE `auteur_secondaire`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `categorie`
---
-ALTER TABLE `categorie`
-  MODIFY `cod_cat` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `date_parution`
---
-ALTER TABLE `date_parution`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `domaine`
---
-ALTER TABLE `domaine`
-  MODIFY `cod_dom` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `domaine_ouvrage`
---
-ALTER TABLE `domaine_ouvrage`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `emprunt`
---
-ALTER TABLE `emprunt`
-  MODIFY `num_emp` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `langue`
---
-ALTER TABLE `langue`
-  MODIFY `cod_lang` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `membre_indelicat`
---
-ALTER TABLE `membre_indelicat`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `ouvrage`
---
-ALTER TABLE `ouvrage`
-  MODIFY `cod_ouv` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `token`
---
-ALTER TABLE `token`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `utilisateur`
---
-ALTER TABLE `utilisateur`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+INSERT INTO `utilisateur` (`id`, `nom`, `prenom`, `sexe`, `date_naissance`, `email`, `telephone`, `mot_de_passe`, `profil`, `avatar`, `est_actif`, `est_supprimer`, `creer_le`, `maj_le`, `email_valide`, `telephone_valide`, `nom_utilisateur`, `adresse`) VALUES
+(30, 'EMEH', 'Restarick', 'M', '1999-10-17', 'emehoceane@gmail.com', 67657013, '05b530ad0fb56286fe051d5f8be5b8453f1cd93f', 'bibliothecaire', '/soutenance/public/image/utilisateur_image/fate state.jpg', 1, 0, '2023-07-11 13:58:14', '2023-07-15 17:56:00', NULL, NULL, 'Restarick EMEH', 'Cotonou'),
+(31, 'EMEH', 'Restarick', 'M', '1999-10-17', 'emehrestarick77@gmail.com', 66057342, '04f081741466827161bede82a374af0ec9a39e31', 'MEMBRE', '/soutenance/public/image/utilisateur_image/FB_IMG_1651251178570.jpg', 1, 0, '2023-07-15 14:03:56', '2023-07-24 08:09:04', NULL, NULL, 'REYES17', 'Cotonou');
 
 --
 -- Contraintes pour les tables déchargées
